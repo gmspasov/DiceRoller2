0ee05ba85ddcffb095f248a619cbd1d6017a9513
georgi.spasov <georgi.spasov@mobisystems.com>
2019-10-31 11:25:14 +0200

    new commit

--- app/src/main/java/com/example/diceroller/DiceView.java
+++ app/src/main/java/com/example/diceroller/DiceView.java
@@ -1,22 +1,10 @@
 package com.example.diceroller;
 
-import android.app.ActionBar;
 import android.content.Context;
-<<<<<<< Updated upstream
-import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.Rect;
-=======
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
->>>>>>> Stashed changes
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,15 +14,10 @@ import androidx.annotation.RequiresApi;
 
 import java.util.Random;
 
+
+
 public class DiceView extends View {
-<<<<<<< Updated upstream
 
-private         Bitmap                  bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice1);
-private Matrix m=new Matrix();
-    ;
-
-=======
->>>>>>> Stashed changes
 
     private Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice1);
     private Matrix m = new Matrix();
@@ -73,27 +56,9 @@ private Matrix m=new Matrix();
 
         switch (randomInt) {
             case 1:
-<<<<<<< Updated upstream
                  bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice1);
                 break;
 
-            case 2:
-                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice2);
-                break;
-
-            case 3:
-                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice3);
-                break;
-
-            case 4:
-                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice4);
-                break;
-
-            case 5:
-                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice5);
-=======
-                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice1);
-                break;
 
             case 2:
                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice2);
@@ -109,7 +74,6 @@ private Matrix m=new Matrix();
 
             case 5:
                 bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.dice5);
->>>>>>> Stashed changes
                 break;
 
             case 6:
@@ -122,7 +86,6 @@ private Matrix m=new Matrix();
     public boolean performClick() {
         invalidate();
         rolldice();
-        invalidate();
         return super.performClick();
     }
 
@@ -130,10 +93,7 @@ private Matrix m=new Matrix();
         super.onDraw(canvas);
         canvas.drawBitmap(bitmap, m, null);
     }
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 }
 
 
